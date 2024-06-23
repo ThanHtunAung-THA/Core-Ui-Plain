e03901a64b35373ce8793606ec6212b44ae9cc35
BuggyCody <31753974+ThanHtunAung-THA@users.noreply.github.com>
2024-06-23 06:44:40 +0630

    content pages testing

--- src/views/pages/content/content.js
+++ src/views/pages/content/content.js
@@ -1,10 +1,16 @@
 import React, { useState } from "react";
+import Content2 from "./content2";
+import Content3 from "./content3";
+
 
 const Content = () => {
 
   return (
     <>
       <h1>this is content page.</h1>
+      <Content2 />
+      <Content3 />
+
     </>
   )
 }
